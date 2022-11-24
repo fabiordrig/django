9d0204202618277b36f257dc9715b3862db4199b
Fabio Rodrigues <fabiordrig@gmail.com>
2022-11-23 21:13:14 -0300

    feat: add font-awesome

---
+++ recipes/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Otávio',
+    })
